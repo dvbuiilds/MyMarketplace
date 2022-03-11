ebef6fc4dab86948af62cce5a53b5382b133ea0a
Sincere-Dhairya <varshneydhairya4@gmail.com>
2022-03-11 23:06:17 +0530

    added hosts

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://dv-store.herokuapp.com/',
+    '127.0.0.1'
+]
 
 
 # Application definition
